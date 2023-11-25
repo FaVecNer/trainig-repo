9bdee6f54807802b9e8c78216137984bc0676e9d
FaVecNer <151643268+FaVecNer@users.noreply.github.com>
2023-11-25 21:14:39 +0700

    15

--- tests/exponentation-test1.js
+++ tests/exponentation-test1.js
@@ -1,4 +1,4 @@
-import { exponentation } from '..calculator/exponentation.js'
+import { exponentation } from './calculator/exponentation.js'
 export function exponentationTest1(n1, n2) {
     let otv = exponentation(n1, n2)
     if (otv === 8) {
