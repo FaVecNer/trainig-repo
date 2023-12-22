68781f22f5c25c13b1ade813227f0858964519b6
FaVecNer <151643268+FaVecNer@users.noreply.github.com>
2023-12-22 18:10:01 +0700

    32

---
+++ calculator/calc.js
@@ -0,0 +1,7 @@
+let a = ''; //first num
+let b = ''; //second num
+let sign = '';//знак операции
+let finish = false;
+
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'];
+const action = ['-', '+', 'X', '/', '^'];
