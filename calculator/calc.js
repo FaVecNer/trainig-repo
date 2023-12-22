ca8fdba51a3f30e605056129458632328994e165
FaVecNer <151643268+FaVecNer@users.noreply.github.com>
2023-12-22 18:14:05 +0700

    33

--- calculator/calc.js
+++ calculator/calc.js
@@ -5,3 +5,14 @@ let finish = false;
 
 const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'];
 const action = ['-', '+', 'X', '/', '^'];
+
+//экран
+const out = document.querySelector('.calc-screen p')
+
+function clearAll() {
+    let a = '';
+    let b = '';
+    let sign = '';
+    let finish = false;
+    our.textContent = 0;
+}
