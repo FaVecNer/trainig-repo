b54c3afd9ce9a49d6becf7dd3b9f78242509b92a
FaVecNer <151643268+FaVecNer@users.noreply.github.com>
2023-12-23 19:59:39 +0700

    32

--- calculator/calc.js
+++ calculator/calc.js
@@ -1,3 +1,4 @@
+import { exponentation } from './Exponentation.js';
 let a = ''; //first num
 let b = ''; //second num
 let sign = '';//знак операции
@@ -6,7 +7,7 @@ const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'];
 const action = ['-', '+', 'X', '/', '^'];
 //экран
 const out = document.querySelector('.calc-screen p')
-import { exponentation } from './Exponentation.js';
+
 function clearAll() {
     a = '';
     b = '';
