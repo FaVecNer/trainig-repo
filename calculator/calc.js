99fa4a606d0db9651d565fd0a1c268fc3b798ee9
FaVecNer <151643268+FaVecNer@users.noreply.github.com>
2023-12-28 23:01:58 +0700

    17

--- calculator/calc.js
+++ calculator/calc.js
@@ -7,7 +7,7 @@ let finish = false;
 const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'];
 const action = ['-', '+', 'X', '/', '^'];
 //экран
-const out = document.querySelector('.calc-screen display')
+const out = document.querySelector('.calc-screen input')
 
 function clearAll() {
     a = '';
